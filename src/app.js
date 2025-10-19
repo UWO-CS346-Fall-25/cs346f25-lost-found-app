0e7a88453d652a87e9126632ac4a052200f62dae
Colin O’Dwanny <colinodwanny@MacBook-Air-32.local>
2025-10-19 09:55:23 -0400

    adding app.listen() to app.js, so I can run the app locally

--- src/app.js
+++ src/app.js
@@ -112,3 +112,9 @@ app.use((err, req, res, _next) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
+
